7f806af04bd3062cb915483d705f353c9fcd12ce
Harris-Ryan <eryanharris1@gmail.com>
2023-02-23 13:52:54 -0700

    testing deploy pipeline with fresh git worktree

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -29,7 +29,7 @@ const Map = ({ ipLocation }: Props) => {
 				/>
 				<Marker position={ipLocation} icon={locationIcon}>
 					<Popup>
-						<span>IP&apos;s Approximate Location</span>
+						<span>Approximate IP Location</span>
 					</Popup>
 				</Marker>
 				<ChangeMapView coords={ipLocation} />
