6d8d5db740dee701ede57f064aa0169f84299bfe
Harris-Ryan <eryanharris1@gmail.com>
2023-02-23 18:05:11 -0700

    changing app content to test ci/cd workflow

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
 	return (
 		<h1 className='font-rubik text-white text-center text-2xl pt-5 md:text-4xl md:pt-10'>
-			IP Address Tracker
+			Changed Header for Test
 		</h1>
 	)
 }
